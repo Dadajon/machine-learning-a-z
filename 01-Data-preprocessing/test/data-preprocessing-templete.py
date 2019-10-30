bda91a421e9047871c673d6c593b0b4668480a6c
Dadajon <dadajonjurakuziev@gmail.com>
2019-10-30 22:42:01 +0900

    Import libraries

---
+++ 01-Data-preprocessing/test/data-preprocessing-templete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 22:13:23 2019
+
+@author: dadajonjurakuziev
+"""
+
+# Data Preprocessing
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ 
