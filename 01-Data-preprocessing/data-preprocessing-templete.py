3a68d872430336640b651a17447f7dda662945cc
Dadajon <dadajonjurakuziev@gmail.com>
2019-10-31 23:24:43 +0900

    Complete #data-preprocessing templete

--- 01-Data-preprocessing/data-preprocessing-templete.py
+++ 01-Data-preprocessing/data-preprocessing-templete.py
@@ -18,31 +18,16 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Handling missing data
-from sklearn.impute import SimpleImputer
-missing_value = SimpleImputer(missing_values=np.nan, strategy='mean', verbose=0)
-missing_value = missing_value.fit(X[:, 1:3])
-X[:, 1:3] = missing_value.transform(X[:, 1:3])
-
-# Encoding categorical data
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(ct.fit_transform(X), dtype=np.float)
-
-# Encoding Y data
-from sklearn.preprocessing import LabelEncoder
-y = LabelEncoder().fit_transform(y)
-
 # Splitting dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
 # Feature Scaling
+"""
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train.reshape(-1,1))
-
+"""
